33bd7414e51b0ca27e894294fc9f31cd2207b0be
Regev Golan <regev@hiredscore.com>
2019-03-14 19:03:44 +0200

    Make game MVP- enable legal moves that change spots and track score (#13)
    This closes #2, closes #3, closes #12

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-var currentTurn = null;
-var mainDiv = null;
-var cells = [];
-let BOARD_SIZE = 8;
 let BLANK = 0;
 let WHITE = 1;
 let BLACK = 2;
+let BOARD_SIZE = 8;
+
+var currentTurnColor = null;
+var mainDiv = null;
+var cells = [];
+var score = [0, 0];
 
 function init(divId) {
      mainDiv = document.getElementById(divId);
      makeBoard();
      initGame();
- }
+}
 
 function makeBoard() {
     var boardTable = document.createElement('table');
@@ -31,8 +33,10 @@ function makeBoard() {
 
 function bindCellClick(i, j, td) {
     td.onclick = function(event) {
-        setSpot(i, j, currentTurn);
-        currentTurn = currentTurn % 2 + 1;
+        is_changed = makeMove(i, j, currentTurnColor);
+        if (is_changed) {
+            currentTurnColor = switchColor(currentTurnColor);
+        }
     }
 }
 
@@ -42,18 +46,81 @@ function initGame() {
     setSpot(BOARD_SIZE/2-1, BOARD_SIZE/2, BLACK);
     setSpot(BOARD_SIZE/2, BOARD_SIZE/2-1, BLACK);
     setSpot(BOARD_SIZE/2, BOARD_SIZE/2, WHITE);
-    setScore(2, 2);
 }
 
-function setTurn(turn) {
-    currentTurn = turn;
+function setTurn(color) {
+    currentTurnColor = color;
 }
 
-function setScore(blackCount, whiteCount) {
-    // TODO: Implement
+function switchColor(color) {
+    return color % 2 + 1;
+}
+
+function updateScore(old_color, new_color) {
+    score[new_color-1]++;
+    if (old_color != BLANK) {
+        score[old_color-1]--;
+    }
+}
+
+function makeMove(i, j, color) {
+    if (isSpotColor(i, j, BLANK)) {
+        cellsToChange = getCellsToChangeByMove(i, j, color);
+        if (cellsToChange.length > 0) {
+            for (changeIndex=0; changeIndex<cellsToChange.length; changeIndex++) {
+                setSpot(cellsToChange[changeIndex][0], cellsToChange[changeIndex][1], color);
+            }
+            return true;
+        }
+    }
+    return false;
 }
 
 function setSpot(i, j, color) {
+    updateScore(cells[i][j].value, color);
     cells[i][j].elem.innerHTML = color;
     cells[i][j].value = color;
+}
+
+function isSpotColor(i, j, color) {
+    return isSpotInBoard(i, j) && cells[i][j].value === color;
+}
+
+function getCellsToChangeByMove(i, j, color) {
+    cellsToChange = [];
+    for (diri=-1; diri<=1; diri++) {
+        nexti = i + diri;
+        for (dirj=-1; dirj<=1; dirj++) {
+            nextj = j + dirj;
+            colorFlushSpots = getColorFlushSpots(switchColor(color), nexti, nextj, diri, dirj);
+            cellsToChange = cellsToChange.concat(colorFlushSpots);
+        }
+    }
+    if (cellsToChange.length > 0) {
+        cellsToChange.push([i, j]);
+    }
+    return cellsToChange
+}
+
+function getColorFlushSpots(color, i, j, diri, dirj) {
+    return getColorFlushSpotsAccumalator([], color, i, j, diri, dirj);
+}
+
+function getColorFlushSpotsAccumalator(spots, color, i, j, diri, dirj) {
+    if (isSpotColor(i, j, switchColor(color))) {
+        return spots;
+    }
+    if (isSpotColor(i, j, color)) {
+        spots.push([i, j]);
+        return getColorFlushSpotsAccumalator(spots, color, i+diri, j+dirj, diri, dirj);
+    }
+    return []
+}
+
+function isSpotInBoard(i, j) {
+    return isNumBetween(i, 0, BOARD_SIZE-1) && isNumBetween(j, 0, BOARD_SIZE-1);
+}
+
+function isNumBetween(x, min, max) {
+    return x >= min && x <= max;
 }
